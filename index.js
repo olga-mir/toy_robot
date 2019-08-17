adf79d685f25c73444686f7134fa2de431b6143f
Olga Mirensky <olga.mirensky@gmail.com>
2019-08-17 14:42:20 +1000

    Restructuring tests

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
-// App
+let response = {
+  myapplicaton: [{
+    version: '1.0',
+    lastcommitsha: 'abcdef01234567890',
+    description: 'ciao come va'
+  }]
+}
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send(response)
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
--- scripts/cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# uninstalle dependencies
-
-npm uninstall inquirer > /dev/null 2>&1 && PID=$!
-wait $PID
-rm -rf node_modules
